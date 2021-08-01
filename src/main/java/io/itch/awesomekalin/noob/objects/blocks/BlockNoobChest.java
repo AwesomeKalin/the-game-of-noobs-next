794a71e775bfe9e09ecd1b0f96abd7d38dff77d0
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2021-08-01 16:30:19 +0100

    Fixed Noob Chest model not working

--- src/main/java/io/itch/awesomekalin/noob/objects/blocks/BlockNoobChest.java
+++ src/main/java/io/itch/awesomekalin/noob/objects/blocks/BlockNoobChest.java
@@ -5,12 +5,14 @@ import io.itch.awesomekalin.noob.Reference;
 import io.itch.awesomekalin.noob.init.BlockInit;
 import io.itch.awesomekalin.noob.init.ItemInit;
 import io.itch.awesomekalin.noob.objects.blocks.noob_chest.TileEntityNoobChest;
+import io.itch.awesomekalin.noob.util.IHasModel;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
@@ -19,7 +21,7 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class BlockNoobChest extends BlockContainer {
+public class BlockNoobChest extends BlockContainer implements IHasModel {
     public BlockNoobChest(String name){
         super(Material.SPONGE);
         setSoundType(SoundType.SLIME);
@@ -33,6 +35,12 @@ public class BlockNoobChest extends BlockContainer {
 
         BlockInit.BLOCKS.add(this);
         ItemInit.ITEMS.add(new ItemBlock(this).setRegistryName(name));
+    }
+
+    @Override
+    public void registerModels()
+    {
+        Main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
     }
 
     @Override
